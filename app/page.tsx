dc480211813bb56de564afde19e93f361b69a51f
KotaHusky <kota@bandr.org>
2023-12-11 01:54:10 -0500

    fix: revert to img
    site is standalone with no Server to process <image />

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default async function Index() {
     <div className="bg-vertical md:bg-horizontal">
       <div className="flex flex-col items-center mx-24 text-center">
         <div className="my-8">
-          <Image src="/images/kota.png" alt="Profile Picture" className="w-32 h-32 md:w-64 md:h-64 rounded-full" width={256} height={256} />
+          <img src="/images/kota.png" alt="Profile Picture" className="w-32 h-32 md:w-64 md:h-64 rounded-full" />
         </div>
         <h1 className="text-3xl font-bold mb-2">Kota Husky</h1>
         <div className="mb-4 flex items-center text-sm text-gray-500">
